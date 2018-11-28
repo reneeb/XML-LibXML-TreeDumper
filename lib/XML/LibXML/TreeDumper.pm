50ef3a3df5d31d79b92f44916345da3706bf9146
reneeb <info@perl-services.de>
2018-11-28 20:29:34 +0100

    prepare release

--- lib/XML/LibXML/TreeDumper.pm
+++ lib/XML/LibXML/TreeDumper.pm
@@ -9,7 +9,7 @@ use Carp;
 use XML::LibXML;
 use Moo;
 
-our $VERSION = 0.02;
+our $VERSION = '0.03';
 
 has data => ( is => 'rw', trigger => \&_parse );
 has node => ( is => 'rw', lazy    => 1, builder => \&_parse);
